4416eca5e204bffad61f75a2e4fc75b633fb6b0c
Serena Inzani <serenainzani@hotmail.com>
2024-02-09 10:50:24 +0000

    removed sensitive comment

--- src/components/Track.js
+++ src/components/Track.js
@@ -25,10 +25,3 @@ export default function Track(props) {
         </>
     );
 }
-
-// curl -X POST "https://accounts.spotify.com/api/token" \
-//      -H "Content-Type: application/x-www-form-urlencoded" \
-//      -d "grant_type=client_credentials&client_id=7e9b1fa4e992419db57c7c003eda9e31&client_secret=1618e23c6f0f4f3b85ee9832db48d03f"
-
-//      curl "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" \
-//      -H "Authorization: Bearer  BQByo0PhnkJS-GQ8wgTrpR0axm2iMXBXQSPcvbF9wIIAc138e5s3g46zINIgWcxfXRimnaFMrYdf4Hi5oMT1mY0dE0yD9X0-B3LN-2MomuSd49qMbEE"
